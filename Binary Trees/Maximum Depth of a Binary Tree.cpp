338e06858b550ea601ee50b497e8d27f2ea5cce5
Surya Ashish <ashishsurya126@gmail.com>
2022-10-21 20:19:20 +0530

    Update Maximum Depth of a Binary Tree.cpp

--- Binary Trees/Maximum Depth of a Binary Tree.cpp
+++ Binary Trees/Maximum Depth of a Binary Tree.cpp
@@ -1,14 +1,18 @@
-#include <bits/stdc++.h>
-#include "BinaryTreeNode.h"
-using namespace std;
-/*
-  Given root of binary tree return its maximum depth
-*/
-
-int maxDepth(BinaryTreeNode<int> *root)
-{
-  if (root == nullptr)
-    return 0;
-
-  return 1 + max(maxDepth(root->left), maxDepth(root->right));
-}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
