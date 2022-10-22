65e202bff118e5ca927f31109bba570da3ceb70a
Surya Ashish <ashishsurya126@gmail.com>
2022-10-22 15:21:37 +0530

    Update Diameter of Binary Tree.cpp

--- Binary Trees/Diameter of Binary Tree.cpp
+++ Binary Trees/Diameter of Binary Tree.cpp
@@ -1,20 +1,30 @@
-#include <bits/stdc++.h>
-#include "BinaryTreeNode.h"
-using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
-int diameter(BinaryTreeNode<int> *root, int &d)
-{
-  if (root == nullptr)
-    return 0;
-  int leftHeight = diameter(root->left, d);
-  int rightheight = diameter(root->right, d);
-  d = max(d, leftHeight + rightheight);
-  return 1 + max(leftHeight, rightheight);
-}
 
-int diameter(BinaryTreeNode<int> *root)
-{
-  int max = 0;
-  diameter(root, max);
-  return max;
-}
+    int diameterOfBinaryTree(TreeNode *root, int &d) {
+      if (root == nullptr)
+        return 0;
+      int leftHeight = diameterOfBinaryTree(root->left, d);
+      int rightheight = diameterOfBinaryTree(root->right, d);
+      d = max(d, leftHeight + rightheight);
+      return 1 + max(leftHeight, rightheight);
+    }
+
+    int diameterOfBinaryTree(TreeNode *root) {
+      int max = 0;
+      diameterOfBinaryTree(root, max);
+      return max;
+    }
+};
