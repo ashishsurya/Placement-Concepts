0d50c2d29b742878e16953ffa5bb25417014f8ed
Surya Ashish <ashishsurya126@gmail.com>
2022-10-22 15:25:16 +0530

    Update ZIg Zag Traversal.cpp

--- Binary Trees/ZIg Zag Traversal.cpp
+++ Binary Trees/ZIg Zag Traversal.cpp
@@ -1,14 +1,22 @@
-#include <bits/stdc++.h>
-#include "BinaryTreeNode.h"
-using namespace std;
-
-vector<vector<int>> zigzagTraversal(BinaryTreeNode<int> *root)
-{
-  vector<vector<int>> zigzag;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> zigzag;
   if (root == NULL)
     return zigzag;
 
-  queue<BinaryTreeNode<int> *> q;
+  queue<TreeNode *> q;
   q.push(root);
   bool LtoR = true; // to ckec whether to add elements in reverse order.
 
@@ -17,7 +25,7 @@ vector<vector<int>> zigzagTraversal(BinaryTreeNode<int> *root)
     vector<int> level(size);
 
     for (int i = 0; i < size;i++) {
-      BinaryTreeNode<int> *front = q.front();
+      TreeNode *front = q.front();
       q.pop();
 
       int index = LtoR ? i : size - i - 1;
@@ -27,18 +35,15 @@ vector<vector<int>> zigzagTraversal(BinaryTreeNode<int> *root)
       if(front->right) {
         q.push(front->right);
       }
+        
+    level[index] = front->val;
+    
 
-      level[index] = front->data;
     }
     zigzag.push_back(level);
     LtoR = !LtoR;
   }
 
   return zigzag;
-}
-
-int main()
-{
-
-  return 0;
-}
+    }
+};
